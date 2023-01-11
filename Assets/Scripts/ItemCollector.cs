607445e9e8d7e1a64086ca8b42bb00de6763c04f
Kerem Cikar <k.bilge3817@gmail.com>
2023-01-11 15:19:49 +0300

    Collection System Done

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {   
     private int money = 0;
     private int cards = 0;
+
+    [SerializeField] private TextMeshProUGUI MoneyCount;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Collectable")){
             Destroy(collision.gameObject);
             money++;
+            MoneyCount.text = "Money: " + money;
         }
     }
 }
