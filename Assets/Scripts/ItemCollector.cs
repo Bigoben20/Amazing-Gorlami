157417262c10285bfb97b02d4e7d22fef55b6212
Kerem Cikar <k.bilge3817@gmail.com>
2023-01-11 15:50:31 +0300

    Collectible Card Added

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -9,13 +9,20 @@ public class ItemCollector : MonoBehaviour
     private int money = 0;
     private int cards = 0;
 
-    [SerializeField] private TextMeshProUGUI MoneyCount;
+    [SerializeField] private TextMeshProUGUI Counter;
+    [SerializeField] private TextMeshProUGUI Counter2;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Collectable")){
+        if(collision.gameObject.CompareTag("Money")){
             Destroy(collision.gameObject);
             money++;
-            MoneyCount.text = "Money: " + money;
+            Counter.text = "Money: " + money + "\n";
+        }
+        else if(collision.gameObject.CompareTag("Cards")){
+            Destroy(collision.gameObject);
+            cards++;
+            Counter2.text = "Cards: " + cards;
         }
     }
 }
